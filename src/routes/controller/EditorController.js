a25c713e667d1b68bb5f66af1c1359cab2d5d69e
1154016697 <1154016697@qq.com>
2018-07-12 17:00:37 +0800

    tune

---
+++ src/routes/controller/EditorController.js
@@ -0,0 +1,15 @@
+"use strict";
+var express = require("express");
+var EditorController = (function () {
+    function EditorController() {
+    }
+    EditorController.getRouter = function () {
+        var router = express.Router();
+        router.get('/', function (req, res, next) {
+            res.render('category/editor/index');
+        });
+        return router;
+    };
+    return EditorController;
+}());
+module.exports = EditorController.getRouter();
